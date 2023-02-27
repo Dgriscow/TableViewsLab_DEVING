857b7a98d8b1bea7e184bd1958d06364e1520940
Dgriscow <dgrischow@students.mchenry.edu>
2023-02-27 13:53:40 -0600

    Initial Commit

---
+++ TableViewsLab_DEVING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewsLab_DEVING
+//
+//  Created by Devin Grischow on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
